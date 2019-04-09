54de998f9de834c5fe7a29ff5deff94d0e8e1ae6
akibkhan1 <akibkhanrt@gmail.com>
2019-04-09 17:42:47 +0600

    Added mutated_genes function and create_gnome function to Process.cpp

--- LineGeneratorMAIN(2)/LineGeneratorMAIN(2)/Process.cpp
+++ LineGeneratorMAIN(2)/LineGeneratorMAIN(2)/Process.cpp
@@ -3,3 +3,22 @@
 #include<string>
 #include<vector>
 using namespace std;
+
+char Process::mutated_genes()
+{
+	const string GENES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP"\
+		"QRSTUVWXYZ 1234567890, .-;:_!\"#%&/()=?@${[]}";
+	int len = GENES.size();
+	int r = random_num(0, len - 1);
+	return GENES[r];
+}
+
+string Process::create_gnome()
+{
+	string TARGET="Line Generator";
+	int len = TARGET.size();
+	string gnome = "";
+	for (int i = 0; i < len; i++)
+		gnome += mutated_genes();
+	return gnome;
+}
