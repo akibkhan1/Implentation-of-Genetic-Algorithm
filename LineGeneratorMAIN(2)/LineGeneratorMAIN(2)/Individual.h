db5b2bf14d0dfe082dc4e253dc613cee5bf5ea88
fardifa <ishma.in008@gmail.com>
2019-04-11 20:17:33 +0600

    Create individual.h

---
+++ LineGeneratorMAIN(2)/LineGeneratorMAIN(2)/Individual.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Individual
+{
+public:
+	string TARGET;
+	string chromosome;
+	int fitness;
+	Individual(string chromosome);
+	Individual mate(Individual parent2);
+	int cal_fitness();
+
+};
