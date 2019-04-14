8cdc4e9c529c3e8e11c73f605899cbf4ca224dd9
Farhan Anjum <farhan.saad10@gmail.com>
2019-04-14 23:47:16 +0600

    Update LineGeneratorMAIN(2).cpp

--- LineGeneratorMAIN(2)/LineGeneratorMAIN(2)/LineGeneratorMAIN(2).cpp
+++ LineGeneratorMAIN(2)/LineGeneratorMAIN(2)/LineGeneratorMAIN(2).cpp
@@ -4,9 +4,23 @@
 #include<vector>
 #include<cstdio>
 #include<algorithm>
-
+using namespace std;
+bool operator<(const Individual& ind1, const Individual& ind2)
+{
+    return ind1.fitness < ind2.fitness;
+}
 int main()
 {
-    
+    string TARGET="LINE GENERATOR";
+    Process ob1[100];
+    int POPULATION_SIZE = 100;
+    int generation = 1;
+
+    vector<Individual> population;
+    for (int i = 0; i < POPULATION_SIZE; i++)
+    {
+        string gnom = ob1[i].create_gnome();
+        population.push_back(Individual(gnom));
+    }
 }
 
