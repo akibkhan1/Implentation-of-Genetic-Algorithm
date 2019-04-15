bda8a433c0c65f92cc6e22330c88412b7b6d230b
fahim52 <47499044+fahim52@users.noreply.github.com>
2019-04-15 10:12:19 +0600

    Added mating constructor to individual.cpp

--- LineGeneratorMAIN(2)/LineGeneratorMAIN(2)/Individual.cpp
+++ LineGeneratorMAIN(2)/LineGeneratorMAIN(2)/Individual.cpp
@@ -10,3 +10,24 @@ Individual::Individual(string chromosome)
 	this->chromosome = chromosome;
 	fitness = cal_fitness();
 };
+Individual Individual::mate(Individual par2)
+{
+	Process ob;
+	string child_chromosome = "";
+
+	int len = chromosome.size();
+	for (int i = 0; i < len; i++)
+	{
+		float p = ob.random_num(0, 100) / 100;
+
+		if (p < 0.35)
+			child_chromosome += chromosome[i];
+
+		else if (p < 0.85)
+			child_chromosome += par2.chromosome[i];
+
+		else
+			child_chromosome += ob.mutated_genes();
+	}
+	return Individual(child_chromosome);
+};
